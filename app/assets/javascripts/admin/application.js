f16fcbac75b94f3ff203e4eaff47b2ab19e8dd78
saasbook <saasbook@saasbook.(none)>
2014-12-24 10:48:14 -0400

    Initial commit for rBlog application

---
+++ app/assets/javascripts/admin/application.js
@@ -0,0 +1,28 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require froala_editor.min.js
+// Include other plugins.
+//= require plugins/block_styles.min.js
+//= require plugins/colors.min.js
+//= require plugins/media_manager.min.js
+//= require plugins/tables.min.js
+//= require plugins/video.min.js
+//= require plugins/font_family.min.js
+//= require plugins/font_size.min.js
+//= require plugins/file_upload.min.js
+//= require plugins/lists.min.js
+//= require plugins/char_counter.min.js
+//= require_tree .
